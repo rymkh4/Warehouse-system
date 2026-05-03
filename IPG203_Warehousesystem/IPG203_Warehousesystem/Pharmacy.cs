ed6052e7c5ee763fa048c324fc81dc9388e18604
rymkh4 <rymtalkhlf00@gmail.com>
2026-05-03 13:02:22 +0300

    add new data member for Pharmacy

--- IPG203_Warehousesystem/IPG203_Warehousesystem/Pharmacy.cs
+++ IPG203_Warehousesystem/IPG203_Warehousesystem/Pharmacy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace IPG203_Warehousesystem
 {
+    // Represents a pharmacy that places orders at the warehouse.
+    // Used to identify who submitted an order and to include the pharmacy's name in event messages.
+
     public class Pharmacy
     {
-        public string PharmacyName { get; set; }
+        private string _pharmacyName;
+        private string _phoneNumber;
+        // Gets or sets the display name of this pharmacy.
+        public string PharmacyName
+        {
+            get { return _pharmacyName; }
+            set { _pharmacyName = value; }
+        }
 
-        public Pharmacy(string name)
+        // Gets or sets the display phone number of this pharmacy.
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+        // Initializes a new Pharmacy with the given name and given number.
+        // "name" is The name of the pharmacy (e.g. "AlShifa Pharmacy").
+        public Pharmacy(string name, string phone)
         {
             PharmacyName = name;
+            PhoneNumber = phone;
         }
     }
 }
